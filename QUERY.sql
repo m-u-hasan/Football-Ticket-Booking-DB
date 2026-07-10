0e765bf07a64e55ab29c5b25697ff390a4662b7b
m-u-hasan <hasan.cse.duet@gmail.com>
2026-07-10 22:50:11 +0600

    Match booking details query add

--- QUERY.sql
+++ QUERY.sql
@@ -189,3 +189,17 @@ FROM
     Bookings
 WHERE
     payment_status IS NULL;
+
+
+    -- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+    b.booking_id, 
+    u.full_name, 
+    m.fixture, 
+    b.total_cost
+FROM 
+    Bookings b
+INNER JOIN 
+    Users u ON b.user_id = u.user_id
+INNER JOIN 
+    Matches m ON b.match_id = m.match_id;
+
